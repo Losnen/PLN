c1ae340bade265c25b8526ab8d412fad84da6f5b
Samuel Ramos <alu0100815146@ull.edu.es>
2016-11-17 23:49:15 +0000

    Añadiendo LSA

--- PLN.R
+++ PLN.R
@@ -1,47 +1,91 @@
 library(tm)
-library(ggplot2)
+library(textreg)
 library(wordcloud)
+library(dplyr)
+library(lsa)
 
+#Leemos los ficheros
 txt <- readLines("/Users/Losnen/Desktop/TIO/PLN-R/txt/MT1.txt",encoding="UTF-8")
 txt2 <- readLines("/Users/Losnen/Desktop/TIO/PLN-R/txt/MT2.txt",encoding="UTF-8")
 txt3 <- readLines("/Users/Losnen/Desktop/TIO/PLN-R/txt/MT3.txt",encoding="UTF-8")
 txt4 <- readLines("/Users/Losnen/Desktop/TIO/PLN-R/txt/MT4.txt",encoding="UTF-8")
 txt5 <- readLines("/Users/Losnen/Desktop/TIO/PLN-R/txt/MT5.txt",encoding="UTF-8")
-txt <- append(txt,txt2)
-txt <- append(txt,txt3)
-txt <- append(txt,txt4)
-txt <- append(txt,txt5)
-
-
 txt = iconv(txt,to="ASCII//TRANSLIT")
+txt2 = iconv(txt2,to="ASCII//TRANSLIT")
+txt3 = iconv(txt3,to="ASCII//TRANSLIT")
+txt4 = iconv(txt4,to="ASCII//TRANSLIT")
+txt5 = iconv(txt5,to="ASCII//TRANSLIT")
+sw <- readLines("/Users/Losnen/Desktop/TIO/PLN-R/stopwords.es",encoding="UTF-8")
+sw = iconv(sw,to="ASCII//TRANSLIT")
 
+#Creamos los corpus
 corpus <- Corpus(VectorSource(txt))
+corpus2 <- Corpus(VectorSource(txt2))
+corpus3 <- Corpus(VectorSource(txt3))
+corpus4 <- Corpus(VectorSource(txt4))
+corpus5 <- Corpus(VectorSource(txt5))
 
+#Para cada fichero le elimino los espacios en blanco, convierto a minuscula, elimino los signos de puntuacion
+#Elimino los numeros, y las stopwords
 d <- tm_map(corpus,content_transformer(tolower))
 d <- tm_map(corpus,stripWhitespace)
 d <- tm_map(d,removePunctuation)
 d <- tm_map(d,removeNumbers)
-
-
 d <- tm_map(d,removeWords,stopwords("spanish"))
-
-sw <- readLines("/Users/Losnen/Desktop/TIO/R/Ejemplo_R/Datos/stopwords.es",encoding="UTF-8")
-sw = iconv(sw,to="ASCII//TRANSLIT")
 d <- tm_map(d,removeWords,sw)
 
-tdm <- TermDocumentMatrix(d)
-m = as.matrix(tdm)
+d2 <- tm_map(corpus2,content_transformer(tolower))
+d2 <- tm_map(corpus2,stripWhitespace)
+d2 <- tm_map(d2,removePunctuation)
+d2 <- tm_map(d2,removeNumbers)
+d2 <- tm_map(d2,removeWords,stopwords("spanish"))
+d2 <- tm_map(d2,removeWords,sw)
 
-wf <- sort(rowSums(m),decreasing = TRUE)
+d3 <- tm_map(corpus3,content_transformer(tolower))
+d3 <- tm_map(corpus3,stripWhitespace)
+d3 <- tm_map(d3,removePunctuation)
+d3 <- tm_map(d3,removeNumbers)
+d3 <- tm_map(d3,removeWords,stopwords("spanish"))
+d3 <- tm_map(d3,removeWords,sw)
 
-dm <- data.frame(word = names(wf), freq = wf)
+d4 <- tm_map(corpus4,content_transformer(tolower))
+d4 <- tm_map(corpus4,stripWhitespace)
+d4 <- tm_map(d4,removePunctuation)
+d4 <- tm_map(d4,removeNumbers)
+d4 <- tm_map(d4,removeWords,stopwords("spanish"))
+d4 <- tm_map(d4,removeWords,sw)
 
-findFreqTerms(tdm,lowfreq = 20)
-wordcloud(dm$word,dm$freq,random.order = FALSE,color = brewer.pal(8,"Pastel1"))
+d5 <- tm_map(corpus5,content_transformer(tolower))
+d5 <- tm_map(corpus5,stripWhitespace)
+d5 <- tm_map(d5,removePunctuation)
+d5 <- tm_map(d5,removeNumbers)
+d5 <- tm_map(d5,removeWords,stopwords("spanish"))
+d5 <- tm_map(d5,removeWords,sw)
 
-#dm[1:10, ] %>%
- # ggplot(aes(word, freq)) +
-  #geom_bar(stat = "identity", color = "black", fill = "#87CEFA") +
-  #geom_text(aes(hjust = 1.3, label = freq)) + 
-  #coord_flip() + 
-  #labs(title = "Diez palabras más frecuentes en la Constitución Española",  x = "Palabras", y = "Número de usos")
+#Creo los vectores de caracteres
+v <- convert.tm.to.character(d)
+v2 <- convert.tm.to.character(d2)
+v3 <- convert.tm.to.character(d3)
+v4 <- convert.tm.to.character(d4)
+v5 <- convert.tm.to.character(d5)
+
+rm(d,d2,d3,d4,d5)
+
+#Creo 5 ficheros temporales con los ficheros limpios.
+td = tempfile()
+dir.create(td)
+write(v, file=paste(td, "D1", sep="/"))
+write(v2, file=paste(td, "D2", sep="/"))
+write(v3, file=paste(td, "D3", sep="/"))
+write(v4, file=paste(td, "D4", sep="/"))
+write(v5, file=paste(td, "D5", sep="/"))
+
+#Calculo la matriz de términos documentos
+termDocMat = textmatrix(td, minWordLength = 1)
+
+#Creo el espcio semántico latente
+spaceLSA = lsa(termDocMat, dims = dimcalc_raw())
+
+#Mouestro el espacio semántico latente como matriz.
+as.textmatrix(spaceLSA)
+
